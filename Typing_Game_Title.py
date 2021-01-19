0683541edba77b98fb345e58cdcc465bd1166f5f
kmo9705 <kmo9705@naver.com>
2021-01-19 15:03:35 +0900

    # 전체적으로 함수로 작성후 실행하는 방식으로 변경 게임 배경음악 삽입 def game_title():   // 타이틀화면 실행 함수   (작성)    ㄴ 버튼 누름 이벤트 발생시 효과음 발생     //sund def game_start():  // 게임 시작 함수              (미병합:작성중) def game_data():  // DB 다루고 출력할 함수 (미작성)    ㄴ

--- Typing_Game_Title.py
+++ Typing_Game_Title.py
@@ -1,66 +1,97 @@
 import pygame
+import sys
+import os
+import random
+import time
+import sqlite3
+import winsound
 
 pygame.init()
 
-# WHITE= (255,255,255)
-# BLACK = (0,0,0)
-# black2 = (30,30,30)
+# 스크린 전체 크기 지정
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT  = 480
 
-# 화면 크기, 타이틀 설정
-WIDTH = 480
-HEIGHT = 480
-screen= pygame.display.set_mode((WIDTH, HEIGHT))
+# 색상 상수
+BLACK = (0, 0, 0)
 
+# pygame 초기화
+pygame.init()
+
+# 스크린 객체 저장
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("타이핑 게임")
+
+# FPS를 위한 Clock 생성
 clock = pygame.time.Clock()
 
-pygame.display.set_caption("타이핑 게임")
-pygame.mouse.set_visible(1)
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((250,250,250))
-screen.blit(background, (0,0))
-pygame.display.flip()
-
-
-# 타이틀
-# background = pygame.image.load("C:/OSS_python/midTerm_typingGame/image/wall.jpg")
-image_title = pygame.image.load('C:/OSS_python/midTerm_typingGame/image/title.png')
-screen.blit(image_title,(15,HEIGHT/4))
-
 # 사운드 재생
-# pygame.mixer.music.load( "background.mp3" )
-# play(loops=0, start=0.0)
+def sound(sound_f, vol, playtime):
+    pygame.mixer.music.load(sound_f)
+    pygame.mixer.music.set_volume(vol) # 1 ~ 0.1
+    pygame.mixer.music.play(playtime)
 
-# 버튼
-start_image = pygame.image.load('C:/OSS_python/midTerm_typingGame/image/Start_button.png').convert_alpha()
-start_button = screen.blit(start_image,(WIDTH/3,250))
-data_image = pygame.image.load('C:/OSS_python/midTerm_typingGame/image/Data_button.png').convert_alpha()
-data_button = screen.blit(data_image,(WIDTH/3+5,300))
-exit_image = pygame.image.load('C:/OSS_python/midTerm_typingGame/image/exit_button.png').convert_alpha()
-exit_button = screen.blit(exit_image,(WIDTH/3+10,350))
-pygame.display.flip()
 
+#################################################
+#                  game_title():                #
+#################################################
+def game_title():
+    # 배경 사운드
+    sound('./sound/openning01.mid', 0.1, -1)
+    pygame.mouse.set_visible(1)
+    background = pygame.Surface(SCREEN.get_size())
+    background = background.convert()
+    background.fill((250,250,250))
+    SCREEN.blit(background, (0,0))
+    pygame.display.flip()
+
+    # background = pygame.image.load("C:/OSS_python/midTerm_typingGame/image/wall.jpg")
+    image_title = pygame.image.load('./image/title.png')
+    SCREEN.blit(image_title,(15,SCREEN_HEIGHT/4))
+
+    # 버튼
+    global start_button, data_button, exit_button
+    start_image = pygame.image.load('./image/Start_button.png').convert_alpha()
+    data_image = pygame.image.load('./image/Data_button.png').convert_alpha()
+    exit_image = pygame.image.load('./image/exit_button.png').convert_alpha()
+
+
+    start_button = SCREEN.blit(start_image,(SCREEN_WIDTH/3,250))
+    data_button = SCREEN.blit(data_image,(SCREEN_WIDTH/3+5,300))
+    exit_button = SCREEN.blit(exit_image,(SCREEN_WIDTH/3+10,350))
+    pygame.display.flip()
+
+    playing = True
+    while playing:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                winsound.PlaySound('./sound/button.wav', winsound.SND_FILENAME)
+                ## if mouse is pressed get position of cursor ##
+                pos = pygame.mouse.get_pos()
+                ## check if cursor is on button ##
+                if start_button.collidepoint(pos):
+                    ## start ##
+                    game_start()
+                    pygame.display.flip()
+                elif data_button.collidepoint(pos):
+                    ## data ##
+                    game_data()
+                elif exit_button.collidepoint(pos):
+                    ## exit ##
+                    playing = False
+                    
+#################################################
+#                  game_start():                #
+#################################################
 def game_start():
     pass
-
+#################################################
+#                  game_data():                 #
+#################################################
 def game_data():
+    # db = open("./resource/data", 'r')
     pass
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            ## if mouse is pressed get position of cursor ##
-            pos = pygame.mouse.get_pos()
-            ## check if cursor is on button ##
-            if start_button.collidepoint(pos):
-                ## start ##
-                game_start()
-                pygame.display.flip()
-            elif data_button.collidepoint(pos):
-                ## data ##
-                game_data()
-            elif exit_button.collidepoint(pos):
-                ## exit ##
-                run = False
+#### MAIN #####
+game_title()
 pygame.quit()
